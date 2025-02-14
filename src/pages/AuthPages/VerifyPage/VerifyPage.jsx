421df615bd81a41ed2ee1098551cae998b151743
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2025-02-14 17:41:26 +0530

    update code

--- src/pages/AuthPages/VerifyPage/VerifyPage.jsx
+++ src/pages/AuthPages/VerifyPage/VerifyPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import {Link} from "react-router-dom";
+import {Button} from "@mui/material";
+import "./verify.scss";
 const VerifyPage = () => {
-  return <div>asdfsdf</div>;
+  return (
+    <div>
+      <div className="verify_container">
+        <section className="verify_box">
+          <div className="verify_gif"></div>
+
+          <article className="verify">
+            <h1> Your email has been verified</h1>
+            <p>Your email address has been verified. please go on login page</p>
+            <Button id="one">
+              <Link to="/login">Go to Login</Link>
+            </Button>
+          </article>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyPage;
